e241f230b27493e78e19af6ddba47fee2ab10116
sew98 <sewmi98@gmail.com>
2021-09-19 17:21:09 +0530

    create db connection

--- index.js
+++ index.js
@@ -2,21 +2,43 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql");
 
-const db = mysql.createPool({
+
+const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "password",
     database: "qcrs_database",
-});
+    port: "3306"
+})
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('mysql connected..');
+})
+
+//test
+// db.query("INSERT INTO qcrs_database.user VALUES (3,'test','test@test.com','1234567',0716666666,'testtest','test')", (err, rows) => {
+//     if (err) {
+//         throw err;
+//     }
+//     else {
+//         console.log('data sent')
+//         console.log(rows)
+//     }
+// })
+
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-    // const sqlInsert =
-    // "INSERT INTO qcrs_database.user VALUES (2,'test','test@test.com','1234567',0716666666,'testtest','test');"
-    // db.query(sqlInsert, (err, result) => {
-    //     res.send("hello..");
-    // }); 
+
+    res.send("hello..");
 });
 
-app.listen(3001, () => {
-    console.log("running on port 3001")
-});
+
+// app.listen(3001, () => {
+//     console.log("running on port 3001")
+// });
+app.listen(port)
+console.log("app is listening on " + port)
