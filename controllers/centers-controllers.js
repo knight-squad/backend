a7a83ac1d2ddd729e1c8fd57cf4b48a2bc6aaf0b
hiruni25 <hirudesilva25@gmail.com>
2021-10-11 03:46:30 +0530

    Implemented Get All Centers function

--- controllers/centers-controllers.js
+++ controllers/centers-controllers.js
@@ -14,5 +14,19 @@ exports.createCenter = async(req, res, next) => {
     })
 }
 
+// Get all centers => /centers
+exports.getCenters = async(req, res, next) => {
+
+    const centers = await Center.findAll();
+
+    res.status(200).json({
+        success: true,
+        count: centers.length,
+        centers
+    })
+}
+
+// Get single center details = /centers/center/:id
+
 
 
--- routes/centers-routes.js
+++ routes/centers-routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
 const centersController = require('../controllers/centers-controllers');
 
 
 router.post('/new', centersController.createCenter);
+
+router.get('', centersController.getCenters);
 
 
 
